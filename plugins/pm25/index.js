69b7dbd4b772a4f5fef4bdfa653b46c306a5a45b
zenghongtu <zenghongtu@gmail.com>
2020-03-13 22:07:16 +0800

    fix:  plugin pm25

--- plugins/pm25/index.js
+++ plugins/pm25/index.js
@@ -2,7 +2,11 @@ module.exports = () => {
   // From https://api.isoyu.com/#/?id=_5-%e5%a4%a9%e6%b0%94%e9%a2%84%e6%8a%a5
   (() => {
     ppet.showMessage('[pm25] 查询中...', 10000, 12);
-    fetch('https://api.isoyu.com/api/Weather/get_weather?city=上海市')
+    fetch(
+      `https://api.isoyu.com/api/Weather/get_weather?city=${encodeURIComponent(
+        '上海市'
+      )}`
+    )
       .then(response => response.json())
       .then(data => {
         ppet.showMessage(
