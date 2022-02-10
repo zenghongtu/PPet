09a4fdceb3b743c00e35c34ba59b95b9a1d375e5
zenghongtu <zenghongtu@gmail.com>
2022-02-10 18:07:10 +0800

    fix: use hash router

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 )
